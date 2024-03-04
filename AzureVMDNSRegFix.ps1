fe0094ac2a65e8a48f637634ef703c54266e1c64
hoxtonist <55714309+hoxtonist@users.noreply.github.com>
2024-03-04 11:29:55 +0000

    Update AzureVMDNSRegFix.ps1
    Added where-object to get-wmiobject

--- AzureVMDNSRegFix.ps1
+++ AzureVMDNSRegFix.ps1
@@ -1,5 +1,5 @@
 $domainName = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select-Object Domain
-$networkConfigStatus = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'"
+$networkConfigStatus = Get-WmiObject Win32_NetworkAdapterConfiguration -filter "ipenabled = 'true'" | Where-Object {$_.DefaultIPGateway -ne $null}
 $regdns = $false
 foreach ($nic in $networkConfigStatus) {
 
